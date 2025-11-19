188890dd9f2371f12e8370f82cb31a204ed927c9
woolisbest <woolisbest.4520@gmail.com>
2025-11-19 21:17:45 +0900

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html　lang="ja">
     <head><meta charset="UTF-8"></head>
+    <body>
 <div id="visitors">Loading...</div>
 
 <script>
-fetch("https://ip-counter-xxxxx.onrender.com")
+fetch("https://wool-counter.onrender.com/")
   .then(r => r.json())
   .then(d => {
     document.getElementById("visitors").textContent =
       "ユニーク訪問者数: " + d.uniqueVisitors;
   });
 </script>
+    </body>
 </html>
